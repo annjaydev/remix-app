d9cb6cc9ae3d9ef62e0f195273fe0e23a87a0677
Anna Dzhandigova <dzhandigova.exceedteam@gmail.com>
2022-10-05 12:49:45 +0300

    fix: code refactoring

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,28 +1,26 @@
 import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import { FC } from 'react';
 
-import ScrollToTop from '@/shared/ScrollToTop/ScrollToTop';
 import { Home } from '../Home';
 import { Layout } from '../Layout';
 import { Remixes } from '../Remixes';
 import { NotFound } from '../NotFound';
+import { ScrollToTop } from '@/shared';
 
-const AppRoutes: FC = () => {
-  return (
-    <BrowserRouter>
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<Navigate to="/remix-app" replace />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/remix-app/" element={<Layout />}>
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="/remixes" element={<Layout />}>
-            <Route index element={<Remixes />} />
-          </Route>
-        </Routes>
-      </ScrollToTop>
-    </BrowserRouter>
-  );
-};
+const AppRoutes: FC = () => (
+  <BrowserRouter>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Navigate to="/remix-app" replace />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/remix-app/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/remixes" element={<Layout />}>
+          <Route index element={<Remixes />} />
+        </Route>
+      </Routes>
+    </ScrollToTop>
+  </BrowserRouter>
+);
 export default AppRoutes;
